c53c9950fe0c7c27dd1f7f106c90dbcf4597c137
Manuel Varo <manuel.vr46@gmail.com>
2022-10-07 16:38:48 +0200

    getBooks API
    Created the getBooks API and, fixing folders

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const booksRoutes = require("./src/books/routes");
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use("/api/books", booksRoutes);
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
