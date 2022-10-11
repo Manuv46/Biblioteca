cf6d0b519eaab3d67fc27e2af46e5af2c9de1a4b
Manuel Varo <manuel.vr46@gmail.com>
2022-10-11 18:30:21 +0200

    Modify some error handling
    fix error handling, add some error message and add some comment

--- src/books/controller.js
+++ src/books/controller.js
@@ -1,29 +1,41 @@
 const pool = require('../../database/db');
 const queries = require('./queries');
 
-//TODO: ERROR HANDLING
+//Return all the books on the library
 const getBooks = (req, res) => {
     pool.query(queries.getBooks, (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows);
+        if (error) throw error
+        if (!results.rows.length) { //if there are no books send message 404
+            res.status(404).send("There are no books!")
+        } else {
+            res.status(200).json(results.rows);
+        }
     });
 };
 
 
-//TODO: ERROR HANDLING
+//Return all the books on rent, and the information about the user
 const getRentedBooks = (req, res) => {
     pool.query(queries.getRentedBooks, (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows);
+        if (error) throw error
+        if (!results.rows.length) { //if there are no books send message 404
+            res.status(404).send("No rental books!")
+        } else {
+            res.status(200).json(results.rows);
+        }
     });
 };
 
 
-//TODO: ERROR HANDLING
+//Return all the books that are available on the library
 const getAvailableBooks = (req, res) => {
     pool.query(queries.getAvailableBooks, (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows)
+        if (error) throw error
+        if (!results.rows.length) { //if there are no books send message 404
+            res.status(404).send("No available books!")
+        } else {
+            res.status(200).json(results.rows)
+        }
     })
 }
 
--- src/rental/controller.js
+++ src/rental/controller.js
@@ -1,29 +1,37 @@
 const pool = require('../../database/db');
 const queries = require('./queries');
 
+//Returns all active rentals
 const getRentals = (req, res) => {
     pool.query(queries.getRentals, (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows);
+        if (error) throw error
+        if (!results.rows.length) { //if there are no rental send message 404
+            res.status(404).send("No rental")
+        } else {
+            res.status(200).json(results.rows);
+        }
     });
 };
 
+//Add a new Rental
 const addRental = (req, res) => {
-    const { users_id, isbn } = req.body;
-    pool.query(queries.getRentalById, [users_id], (error, results) => {
-        if (results.rows.length != 0) {
+    const { users_id, isbn } = req.body; //the information for the rental be pass by a .JSON file on body
+    pool.query(queries.getRentalById, [users_id], (error, results) => { //Check if the user already has a book on loan
+        if (error) throw error
+        if (results.rows.length != 0) { //If true send back a Forbiden message 
             res.status(403).send("The user already has a book on loan!")
         }
-        else {
+        else {  //Check if the book its available
             pool.query(queries.getRentalByISBN, [isbn], (error, results2) => {
-                if (results2.rows.length != 0) {
+                if (error) throw error
+                if (results2.rows.length != 0) { //If not send back a Forbiden message
                     res.status(403).send("Book already on loan!")
                 }
-                else {
+                else { //Add the new rental
                     pool.query(queries.addRental, [users_id, isbn], (error, results3) => {
-                        if (error) {
+                        if (error) { //If userId or isbn not exist on the library database it's not possible to add the new rental
                             res.status(400).send("User_Id or Book ISBN not exist!")
-                        } else {
+                        } else { //all works fine and the rental is add successfully
                             res.status(201).send("Rental success!!")
                         }
                     });
@@ -34,16 +42,18 @@ const addRental = (req, res) => {
 };
 
 const removeRentedBook = (req, res) => {
-    const { users_id, isbn } = req.body;
-    pool.query(queries.getRentalByIdAndISBN, [users_id, isbn], (error, results) => {
+    const { users_id, isbn } = req.body; //the information for the rental be pass by a .JSON file on body
+    pool.query(queries.getRentalByIdAndISBN, [users_id, isbn], (error, results) => { //check that the rental exist
         const noRentalFound = !results.rows.length;
-        if (noRentalFound) {
+        if (noRentalFound) { //Rental not found, send back 404 message 
             res.status(404).send("The user or the book does not have a rental.");
         }
-        else {
+        else { //Delete the rental
             pool.query(queries.removeRental, [users_id, isbn], (error, results) => {
                 if (error) throw error;
-                res.status(200).send("Rental Deleted Successfully! \n Rental finished, book available");
+                else { //Rental Deleted Successfully
+                    res.status(200).send("Rental Deleted Successfully! \n Rental finished, book available");
+                }
             });
         }
     });
